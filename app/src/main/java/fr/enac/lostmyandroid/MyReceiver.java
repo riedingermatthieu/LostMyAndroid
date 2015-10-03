f3b777650cf292c09f4da6a92fadc279425e1877
amine.boulbars <amine.boulbars@gmail.com>
2015-10-03 15:44:45 +0200

    Generic empty Broadcast Receiver, with OnReceive() method implemented

---
+++ app/src/main/java/fr/enac/lostmyandroid/MyReceiver.java
@@ -0,0 +1,20 @@
+package fr.enac.lostmyandroid;
+
+
+
+import android.content.BroadcastReceiver;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            // TODO Implement SMS Receiving behaviour
+        }
+    }
+}
