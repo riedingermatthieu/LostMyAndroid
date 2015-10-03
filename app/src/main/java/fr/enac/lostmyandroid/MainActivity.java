554a80a89d34de296143bc11bb8fac8fdfd0c208
amine.boulbars <amine.boulbars@gmail.com>
2015-10-03 16:44:20 +0200

    Connecting UI components for later EventListening

--- app/src/main/java/fr/enac/lostmyandroid/MainActivity.java
+++ app/src/main/java/fr/enac/lostmyandroid/MainActivity.java
@@ -5,13 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText number;
+    private Button ringIt;
+    private Button vocalMessage;
+    private Button textMessage;
+
+    private Switch numero;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Loading components
+        number = (EditText) findViewById(R.id.editNumero);
+        ringIt = (Button) findViewById(R.id.buttonRing);
+        vocalMessage = (Button) findViewById(R.id.buttonVocalMessage);
+
 
     }
 
