b591017aa243ecdc7444920265f81900da7fd262
amine.boulbars <amine.boulbars@gmail.com>
2015-10-03 16:22:34 +0200

    Manifest registration of the BroadcastReceiver

--- app/src/main/java/fr/enac/lostmyandroid/MainActivity.java
+++ app/src/main/java/fr/enac/lostmyandroid/MainActivity.java
@@ -1,7 +1,8 @@
 package fr.enac.lostmyandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
