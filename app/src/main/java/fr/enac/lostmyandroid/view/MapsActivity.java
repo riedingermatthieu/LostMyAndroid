1e54f0a732623f6c78da446a88ad2861135b00bb
amine.boulbars <amine.boulbars@gmail.com>
2015-10-30 13:06:15 +0100

    Implémentation de la localisation et pointage MAP

--- app/src/main/java/fr/enac/lostmyandroid/view/MapsActivity.java
+++ app/src/main/java/fr/enac/lostmyandroid/view/MapsActivity.java
@@ -1,41 +1,82 @@
 package fr.enac.lostmyandroid.view;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 
 import fr.enac.lostmyandroid.R;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private Location location;
+
+    public static final String TAG = MapsActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        buildGoogleApiClient();
         setUpMapIfNeeded();
+    }
+
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
      * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
+     * <p/>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
      * install/update the Google Play services APK on their device.
-     * <p>
+     * <p/>
      * A user can return to this FragmentActivity after following the prompt and correctly
      * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
      * have been completely destroyed during this process (it is likely that it would only be
@@ -49,21 +90,95 @@ public class MapsActivity extends FragmentActivity {
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                System.out.print("Set up map");
+            if (mMap != null)
                 setUpMap();
 
-            }
         }
     }
 
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
-     * <p>
+     * <p/>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        int i = 0;
+
+        mMap.setMyLocationEnabled(true);
+
+        // Getting Current Location
+        location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        mLastLocation
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mLastLocation = getMyLocation();
+
+        if (mLastLocation == null) {
+            // Blank for a moment...
+        }
+        else {
+            handleNewLocation(mLastLocation);
+            showLocation(mLastLocation);
+        };
+
     }
+
+
+    //TODO Get position of the device when receiving an SMS - Poke Matthieu
+    /**
+     * Look for the current device's location and returns it
+     * @return Location
+     */
+    public Location getMyLocation(){
+        Context context = getApplicationContext();
+
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+
+        return lastKnownLocation;
+    }
+
+    /**
+     * Add a marker on the map
+     * @param deviceLoc localisation to mark on the map
+     *
+     */
+    public void showLocation(Location deviceLoc)
+    {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(deviceLoc.getLatitude(),
+                deviceLoc.getLongitude())).title("Device"));
+    }
+
+
+    // Non functionnal ///////////////////////////////////////////////////////////////////
+
+
+    private void handleNewLocation(Location loc) {
+        Log.d(TAG, loc.toString());
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        //if (mLastLocation != null) {
+        //FIXME récuparation des coordonnées
+        //mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+        //mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        //}
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i("ErreurAuth", connectionResult.getErrorMessage());
+    }
+
+
 }
